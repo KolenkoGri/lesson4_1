1f7cd3f79148458d6c2a42c9ea304fd9b9b31d1c
KolenkoGri <130161094+KolenkoGri@users.noreply.github.com>
2023-05-02 11:45:47 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const product = "Навигационная система Soundmax";
+const count = 5;
+const category = "Техника для дома";
+const price = "100$";
+
+console.log("Общая сумма товара" , price);
